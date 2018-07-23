5d672c716bc7b7fc67474b8e93d13a358108c3ed
root <root@doa03.localdomain>
2018-07-23 21:22:09 +0700

    exercises Day 13 Area Of A Circle

---
+++ areaOfaCircle.js
@@ -0,0 +1,11 @@
+function square (number) {
+    return number*number;
+  }
+  
+  function pi () {
+    return 3.14;
+  }
+  
+  
+  var area = Math.round(pi() * square(7));;
+  console.log (area); // 154
